46baafbdf1683518a07deb0d768003bae473f5ff
LeHuuHai <le31052003@gmail.com>
2026-03-04 09:38:01 +0700

    add interface: embedd

--- interface/interface/interface.go
+++ interface/interface/interface.go
@@ -21,7 +21,38 @@ func Play(s Speaker) {
 	s.Speak()
 }
 
+// embedd interface
+type Mover interface {
+	Move()
+}
+
+// interface này chứa chữ kí của cả 2 phương thức
+type Animal interface {
+	Speaker
+	Mover
+}
+
+type Dog struct {
+}
+
+// impl
+func (d *Dog) Speak() {
+	fmt.Println("woof woof")
+}
+
+func (d *Dog) Move() {
+	fmt.Println("Dog is running")
+}
+
+func HaveAPet(a Animal) {
+	a.Speak()
+	a.Move()
+}
+
 func main() {
 	cat := Cat{}
 	Play(&cat) // meow meow, *Cat đã impl Speaker
+
+	dog := Dog{}
+	HaveAPet(&dog) // *Dog đã impl Animal
 }
